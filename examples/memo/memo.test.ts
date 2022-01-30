7fc9257bdd704ff93e95d1217bb773deb82e10b9
HoseungJang <hsjang.dev@gmail.com>
2022-01-31 01:02:42 +0900

    sub 메소드 추가

--- examples/memo/memo.test.ts
+++ examples/memo/memo.test.ts
@@ -3,12 +3,21 @@ import { expect } from "chai";
 import { Memo } from "./memo";
 
 export class Calculator {
-  public static calledCount = 0;
+  public static calledCount = {
+    add: 0,
+    sub: 0,
+  };
 
   @Memo()
   public static add(a: number, b: number): number {
-    this.calledCount++;
+    this.calledCount.add++;
     return a + b;
+  }
+
+  @Memo()
+  public static sub(a: number, b: number): number {
+    this.calledCount.sub++;
+    return a - b;
   }
 }
 
@@ -17,10 +26,17 @@ describe("memo", () => {
     it("should be called once with same parameters", () => {
       expect(Calculator.add(1, 2)).to.be.eq(3);
       expect(Calculator.add(1, 2)).to.be.eq(3);
-      expect(Calculator.calledCount).to.be.eq(1);
+      expect(Calculator.calledCount.add).to.be.eq(1);
       expect(Calculator.add(2, 3)).to.be.eq(5);
       expect(Calculator.add(2, 3)).to.be.eq(5);
-      expect(Calculator.calledCount).to.be.eq(2);
+      expect(Calculator.calledCount.add).to.be.eq(2);
+
+      expect(Calculator.sub(5, 3)).to.be.eq(2);
+      expect(Calculator.sub(5, 3)).to.be.eq(2);
+      expect(Calculator.calledCount.sub).to.be.eq(1);
+      expect(Calculator.sub(4, 3)).to.be.eq(1);
+      expect(Calculator.sub(4, 3)).to.be.eq(1);
+      expect(Calculator.calledCount.sub).to.be.eq(2);
     });
   });
 });
